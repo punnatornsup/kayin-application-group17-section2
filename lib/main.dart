8e5111a377604742f054cf2377bc8022310157bc
Punnatorn Supinyo <punnatorn.sup@student.mahidol.edu>
2024-04-16 18:54:43 +0700

    Test01

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Demo',
+      title: 'Login',
       theme: ThemeData(
         primaryColor: const Color.fromARGB(255, 149, 183, 255),
       ),
